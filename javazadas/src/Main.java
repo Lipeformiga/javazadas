5ea7cf8f01c1f2e3e367ec8645e53f8dc48da17b
Filipe Formigari <formigari2804@gmail.com>
2024-10-10 17:00:48 -0300

    Update Main.java

--- javazadas/src/Main.java
+++ javazadas/src/Main.java
@@ -1,13 +1,18 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
 
+        Scanner sc = new Scanner(System.in);
+
         Conta conta1 = new Conta(1,"Romário",80000,50000);
         Conta conta2 = new Conta(2,"Kristian",54000, 30000);
 
+        System.out.println("Digite o valor do saque: ");
+        double valor = sc.nextDouble();
       try {
-           conta1.saque(60000);
+           conta1.saque(valor);
       } catch (SaldoInsuficienteException e){
           System.out.println(e.getMessage());
       } catch (ValorInvalidoException | LimiteInsuficienteException e){
@@ -16,5 +21,49 @@ public class Main {
 
       }
         System.out.println("Ainda está rodando");
+    }
+
+    private void cadastroConta(){
+
+    }
+    private void deletarConta(){
+
+    }
+    private void editarConta(){
+
+    }
+    private void buscarConta(){
+
+    }
+
+    private void login(){
+
+    }
+    private void logout(){
+
+    }
+    private void mostrarDadosConta(){
+
+    }
+    private void mostrarDadosTitular(){
+
+    }
+    private void mostrarOpcoes(){
+
+    }
+    private void cadastroTitular(){
+
+    }
+    private void deletarTitular(){
+
+    }
+    private void editarTitular(){
+
+    }
+    private void buscarTitular(){
+
+    }
+    private void executarOpcao(){
+        
     }
 }

61a22c55b13548f205abbfd4ae339e2c8028655c
Filipe Formigari <formigari2804@gmail.com>
2024-10-10 16:46:07 -0300

    Update Main.java

--- javazadas/src/Main.java
+++ javazadas/src/Main.java
@@ -1,19 +1,20 @@
 import java.util.ArrayList;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args){
 
         Conta conta1 = new Conta(1,"Romário",80000,50000);
         Conta conta2 = new Conta(2,"Kristian",54000, 30000);
 
       try {
            conta1.saque(60000);
-       } catch (SaldoInsuficienteException e){
+      } catch (SaldoInsuficienteException e){
           System.out.println(e.getMessage());
-       } catch (ValorInvalidoException | LimiteInsuficienteException e){
+      } catch (ValorInvalidoException | LimiteInsuficienteException e){
           e.printStackTrace(); // mostra como se tivese crashado a operação
+      } catch (ContaException ignore){
+
       }
         System.out.println("Ainda está rodando");
-
     }
 }
